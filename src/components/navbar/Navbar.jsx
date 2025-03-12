2736721ef8de9d2b2e23f7fc622cfa22f706e05d
KSaayushofficial <ksaayushofficial3@gmail.com>
2025-03-12 23:07:35 +0545

    Setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Links from './links/Links';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>AaYuSh</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
